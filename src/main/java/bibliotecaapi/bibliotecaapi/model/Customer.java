31670c0c9941ceee987fa0a66d12a86cc64dc3c0
Nanda P Caetano <fernandamariacaetanopereira@gmail.com>
2024-11-07 21:07:11 -0300

    Implemetando banco de dados e relacionamento entre tabelas

---
+++ src/main/java/bibliotecaapi/bibliotecaapi/model/Customer.java
@@ -0,0 +1,36 @@
+package bibliotecaapi.bibliotecaapi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Customer{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastname;
+    private String address;
+    private String city;
+    private BigDecimal state;
+    private String country;
+    private LocalDate birthDate;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    
+}
---
+++ src/main/java/bibliotecaapi/bibliotecaapi/model/Status.java
@@ -0,0 +1,5 @@
+package bibliotecaapi.bibliotecaapi.model;
+
+public enum Status {
+    AVAILABLE, BORROWED;
+}
